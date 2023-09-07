dde5669fb298e8027575788e8841e3690ab033f3
raoul2000 <manu34@free.fr>
2023-09-07 10:53:52 +0200

    test: add test for multiple url from plan

--- test/multiple-url.js
+++ test/multiple-url.js
@@ -5,6 +5,7 @@ const bob = require("../index");
 
 describe("when an array of URL must be mined", function () {
     this.timeout(0);
+    
     it("returns an array with a one value per URL unordered", (done) => {
         bob.work(["http://127.0.0.1:8080/blog/post/1.html", "http://127.0.0.1:8080/blog/post/2.html"], "h2")
             .then((result) => {
@@ -16,4 +17,24 @@ describe("when an array of URL must be mined", function () {
             .catch(done);
     });
 
+    it("can use a plan to mine multiple urls", (done) => {
+        bob.work(
+            {
+                url: "http://127.0.0.1:8080/blog/index-link-ok.html",
+                plan: {
+                    selector: ["div.post a"],
+                    type: "@href absolute",
+                },
+            },
+            "h2"
+        )
+            .then((result) => {
+                //console.log(JSON.stringify(result, null, 4));
+                assert.isTrue(result.indexOf("Scraper community : a new kid is on the block") !== -1);
+                assert.isTrue(result.indexOf("Welcome Bob the Web Miner") !== -1);
+                assert.lengthOf(result, 2);
+                done();
+            })
+            .catch(done);
+    });
 });
